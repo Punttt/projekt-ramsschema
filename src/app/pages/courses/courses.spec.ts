b66e5d8188b52bfc30b760de106d378da574b777
Pontus Johansson <johanssonp998@gmail.com>
2026-05-13 12:57:31 +0200

    byter ut profil och lägger till courses och schedule

--- src/app/pages/courses/courses.spec.ts
+++ src/app/pages/courses/courses.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Profile } from './profile';
+import { Courses } from './courses';
 
-describe('Profile', () => {
-  let component: Profile;
-  let fixture: ComponentFixture<Profile>;
+describe('Courses', () => {
+  let component: Courses;
+  let fixture: ComponentFixture<Courses>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Profile],
+      imports: [Courses],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Profile);
+    fixture = TestBed.createComponent(Courses);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
