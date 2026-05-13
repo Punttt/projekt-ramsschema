758c374d2b45dcbaff61055c30139cc7d7695ab5
Pontus Johansson <johanssonp998@gmail.com>
2026-05-13 12:51:54 +0200

    lägger till component för sökning/filter

---
+++ src/app/partials/course-filter/course-filter.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-filter',
+  imports: [],
+  templateUrl: './course-filter.html',
+  styleUrl: './course-filter.scss',
+})
+export class CourseFilter {}
