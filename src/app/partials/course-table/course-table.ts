4d4725a577cb812af8208621f45897164d299c57
Pontus Johansson <johanssonp998@gmail.com>
2026-05-13 12:52:16 +0200

    lägger till component för schedule table

---
+++ src/app/partials/course-table/course-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-table',
+  imports: [],
+  templateUrl: './course-table.html',
+  styleUrl: './course-table.scss',
+})
+export class CourseTable {}
