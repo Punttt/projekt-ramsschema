b11d5e56c05486a55106a84ed96e7885d9a58735
Pontus Johansson <johanssonp998@gmail.com>
2026-05-20 16:08:55 +0200

    uppdaterar courses, anropar func när användaren använder filtrering

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -2,18 +2,48 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CourseFilter } from "../../partials/course-filter/course-filter";
 import { CourseTable } from "../../partials/course-table/course-table";
 import { CourseService } from '../../services/course';
-import { Course } from '../../models/course';
-import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
-  imports: [CourseFilter, CourseTable, AsyncPipe],
+  imports: [CourseFilter, CourseTable],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
 
 export class Courses {
   private courseService = inject(CourseService);
-  courses$: Observable<Course[]> = this.courseService.getCourses();
+
+  // Filtervärden
+  searchText = "";
+  subjectText = "";
+
+  // Hela kurslistan från service
+  allCourses = this.courseService.courses;
+
+  // Filtrerad lista - beräknas om varje gång den läses
+  get filteredCourses() {
+    const search = this.searchText.toLowerCase();
+    const subject = this.subjectText.toLowerCase();
+
+    return this.allCourses().filter(course => {
+      const matchSearch = !search
+        || course.courseCode.toLowerCase().includes(search)
+        || course.courseName.toLowerCase().includes(search);
+
+      const matchSubject = !subject
+        || course.subject.toLowerCase().includes(subject);
+
+      return matchSearch && matchSubject;
+    });
+  }
+
+  // Anropar när använddaren skriver i sökfältet
+  onSearchChange(value:string): void {
+    this.searchText = value;
+  }
+
+  // Anropas när användaren skriver i änmnesfältet
+  onSubjectChange(value: string): void {
+    this.subjectText = value;
+  }
 }
