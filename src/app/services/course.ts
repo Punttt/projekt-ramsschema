ac0db258aff8f124c939d11b77135a6cc95655f9
Pontus Johansson <johanssonp998@gmail.com>
2026-05-20 15:37:38 +0200

    uppdaterar service med signaler

--- src/app/services/course.ts
+++ src/app/services/course.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable, signal, Signal } from '@angular/core';
 import { Course } from '../models/course';
 
 @Injectable({
@@ -10,7 +9,19 @@ export class CourseService {
   private http = inject(HttpClient);
   private jsonUrl = "/miun_courses.json";
 
-  getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>(this.jsonUrl);
-  }
+    // Privat signal som håller listan
+    private coursesSignal = signal<Course[]>([]);
+
+    // Publikt readonly signal - kan läsas av komponenter
+    courses: Signal<Course[]> = this.coursesSignal.asReadonly();
+
+    constructor() {
+        this.loadCourses();
+    }
+
+    private loadCourses(): void {
+        this.http.get<Course[]>(this.jsonUrl).subscribe(data => {
+            this.coursesSignal.set(data);
+        });
+    }
 }
