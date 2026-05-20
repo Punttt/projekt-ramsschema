54185786e20f054f88616adc346096d98ac85c0e
Pontus Johansson <johanssonp998@gmail.com>
2026-05-20 15:30:08 +0200

    testar async pipe för laddning av data

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -3,20 +3,17 @@ import { CourseFilter } from "../../partials/course-filter/course-filter";
 import { CourseTable } from "../../partials/course-table/course-table";
 import { CourseService } from '../../services/course';
 import { Course } from '../../models/course';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
-  imports: [CourseFilter, CourseTable],
+  imports: [CourseFilter, CourseTable, AsyncPipe],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
-export class Courses implements OnInit{
-  private courseService = inject(CourseService);
-  courses: Course[] = [];
 
-  ngOnInit(): void {
-    this.courseService.getCourses().subscribe(data => {
-      this.courses = data;
-    });
-  }
+export class Courses {
+  private courseService = inject(CourseService);
+  courses$: Observable<Course[]> = this.courseService.getCourses();
 }
